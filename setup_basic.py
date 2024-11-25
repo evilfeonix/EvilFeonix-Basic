d7102c6f82d426b7d934aedc96bb2ec223dfdee6
Evil feoniX <evilfeonix@gmail.com>
2024-11-25 09:54:07 +0100

    Update setup_basic.py

--- setup_basic.py
+++ setup_basic.py
@@ -9,29 +9,28 @@ import os,sys,time,subprocess
 # Email: evilfeonix@gmail.com 
 
 
-################ This is a beginners  tool based on cracking email password
-###############  Named, (emailCracker) and was build with the help of python programming language
-######           This tool generates a list of password (also known as wordlist) based on the victim's informatiom provided
-############     Those password this tool generate are 4 in minimum and 18 in maximum (length)
-################ And also trys to crack the victim's email password with the generated password.
+########    EvilFeonix Basic, a basic Python script that aimed at beginners starting with gray hat hacking in Termux. 
+########    This tool install essential Python libraries and Termux packages, 
+########    giving you a foundational toolkit for exploring network security and gray hat hacking.
+########    EvilFeonix Basic provides a setup for installing essential tools and Python packages for gray hat hacking and security analysis on Termux. 
+########    This is ideal for beginners, as it covers network and security fundamentals in a user-friendly manner.
 
 
 stop="\033[0m"
 red="\033[31;1m"
-d_red="\033[91m"
 blue="\033[34;1m"
 green="\033[32;1m"
+darkred="\033[91m"
 yellow="\033[33;1m"
 purple="\033[35;1m"
 
 
 error=f"{blue}[{stop}-{blue}]{red} "
-info=f"{green}[{stop}+{green}]{purple} "
 info2=f"{red}[{stop}•{red}]{yellow} "
 notice=f"{blue}[{stop}!{blue}]{red} "
+info=f"{green}[{stop}+{green}]{purple} "
 first= f"{green}[{stop}01{green}]{purple} "
 second= f"{green}[{stop}02{green}]{purple} "
-third= f"{green}[{stop}03{green}]{purple} "
 version="1.0.0"
 
 
@@ -50,38 +49,22 @@ def internet():
     except Exception:return False
     
 
-def aboutus():
-    os.system("clea" + "r || cls")
-    start_banner()
-    slow(f'\t{info}Tool Name      {blue}:>>{purple}  EVILFEONIX BASIC')
-    slow(f'\t{info}Version        {blue}:>>{purple}  v[{version[:-2]}]')
-    slow(f'\t{info}Author         {blue}:>>{d_red}  FEONIX')
-    slow(f'\t{info}Github         {blue}:>>{purple}  Evil {d_red}FeoniX')
-    slow(f'\t{info}Youtube        {blue}:>>{purple}  Evil {d_red}FeoniX')
-    slow(f'\t{info}Latest Update  {blue}:>>{purple}  13-11-2024')
-    slow(f'\t{info}Website        {blue}:>>{purple}  www.evilfeonix.com')
-    slow(f"{red}")
-    slow("="*60)
-    slow("")
-    act=input(f'{info}Press {purple}[{stop}ENTER{purple}]{yellow} To Continue')
-    return main()
-
 def start_banner():
-    logo=f"""███████╗██╗       ██╗██╗██╗      ███████╗███████╗ ██████╗ ███╗   ██╗██╗██╗  ██╗ 
-{green}██╔════╝ ██║     ██║ ██║██║      ██╔════╝██╔════╝██╔═══██╗████╗  ██║██║╚██╗██╔╝
+    logo=f"""{stop}███████╗██╗       ██╗██╗██╗      ███████╗███████╗ ██████╗ ███╗   ██╗██╗██╗  ██╗ 
+{green}██╔════╝ ██║     ██║ ██║██║      ██╔════╝██╔════╝██╔═══██╗████╗  ██║██║╚██╗██╔╝{stop}
 {green}█████╗    ██║   ██║  ██║██║ ███╗ █████╗  █████╗  ██║   ██║██╔██╗ ██║██║ ╚███╔╝{stop}
 {green}██╔══╝     ██║ ██║   ██║██║ ╚══╝ ██╔══╝  ██╔══╝  ██║   ██║██║╚██╗██║██║ ██╔██╗{stop}
 ███████╗   ╚███╔╝    ██║███████╗ ██║     ███████╗╚██████╔╝██║ ╚████║██║██╔╝ ██╗
 ╚══════╝    ╚══╝     ╚═╝╚══════╝ ╚═╝     ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝ 
                         
 \t\t\t\t██████╗  █████╗ ███████╗██╗ ██████╗
-{blue}:>> {green}Coded by EVIL {d_red}FEONIX{blue} <<:\t{green}██╔══██╗██╔══██╗██╔════╝██║██╔════╝
+{blue}:>> {green}Coded by EVIL {darkred}FEONIX{blue} <<:\t{green}██╔══██╗██╔══██╗██╔════╝██║██╔════╝
 {blue}:>> {green}www.evilfeonix.com{blue} <<:\t{green}██████╔╝███████║███████╗██║██║{stop}   
 {blue}:>> {green}EvilFeonix Basic{blue} <<:\t{green}██╔══██╗██╔══██║╚════██║██║██║{stop}   
 \t      v[{green}{version[:-2]}{stop}]\t\t██████╔╝██║  ██║███████║██║╚██████╗ 
-\t\t\t\t╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝ ╚═════╝\n"""
+\t\t\t\t╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝ ╚═════╝\n{red}"""
     print(logo)
-    slow("============================================================")
+    slow(f"===========================================================")
 
 
 
@@ -125,11 +108,11 @@ def configure_ssh():
 def additional_tools_recommendation():
     slow(f"\n{notice}Additional Recommendations:")
     slow(f"{info2}To install Metasploit, run:{stop}")
-    slow("    'pkg install unstable-repo && pkg install metasploit'")
-    slow("Metasploit is an advanced tool that requires responsibility and legal compliance.")
+    slow("    pkg install unstable-repo && pkg install metasploit")
+    slow(f"{yellow}Metasploit is an advanced tool that requires responsibility and legal compliance.")
     slow(f"{info2}To install Nikto (a web server scanner), run:{stop}")
-    slow("    'pkg install nikto'")
-    slow("These tools should be used responsibly and only on systems you have permission to test.\n")
+    slow("    pkg install nikto")
+    slow(f"{yellow}These tools should be used responsibly and only on systems you have permission to test.\n")
 
 def install_net_utils():
     load(f"\n{info}Installing network utilities{stop}")
@@ -151,22 +134,25 @@ def all_in_0ne():
     end_banner()
 
 def main():
-    os.system("clea" + "r || cls"),start_banner()
+    os.system("clea" + "r || cls")
+    start_banner()
     net=internet()
-    if not net:
+    if net:
+    # if not net:
         time.sleep(1)
-        slow(f"\n{error}Please check ur internet connection{stop}")
+        slow(f"\n{error}Please check your internet connection{stop}")
         os.sys.exit()
     try:
-        slow(""f"\n\t{first}About EvilFeonix Basic\n\t{second}Run EvilFeonix Basic\n\t{third}Exit\n""")
+        slow(""f"\n\t{first}Run EvilFeonix Basic\n\t{second}Exit\n""")
         opt=input(""f"{green}[{stop}SELECT OPTION..{green}] {blue}:>>{purple} """)
-        if opt in["1","01","a","A"]:aboutus()
-        elif opt in["2","02","b","B"]:all_in_0ne()
-        elif opt in["3","03","c","C"]:slow(f"\n{error}Thanks for using our tool.{stop}"),os.sys.exit()
-        else:load(f"\n{error}Invali Option!{stop}"),main()
-    except KeyboardInterrupt:slow(f"\n\n{error}User Requested an Interrupt!{stop}"),load(f"{error}Program Running Down!{stop}"),os.sys.exit()
+        if opt in["1","01","a","A"]:all_in_0ne()
+        elif opt in["2","02","b","B"]:slow(f"\n{error}Thanks for using our tool.{stop}"),os.sys.exit()
+        else:load(f"\n{error}Invalid Option!{stop}"),main()
+    except KeyboardInterrupt:
+        slow(f"\n\n{error}User Requested an Interrupt!{stop}")
+        load(f"{error}Program Running Down!{stop}")
+        os.sys.exit()
 
 
 if __name__ == "__main__":
     main()
-    
